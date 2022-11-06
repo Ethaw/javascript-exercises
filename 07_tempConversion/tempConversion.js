9f3243880a277738b1c1cff3543f8795cd7a8331
Ethaw <12903109+Ethaw@users.noreply.github.com>
2022-11-06 04:29:46 +0100

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(x) {
+  // toFixed returns a string, the unary + converts to number
+  // Warning: round() and toFixed() do not give the same results!
+  // https://stackoverflow.com/questions/566564/math-roundnum-vs-num-tofixed0-and-browser-inconsistencies
+  return +((x - 32) * 5/9).toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(x) {
+  return +(x * 9/5 + 32).toFixed(1);
 };
 
-// Do not edit below this line
 module.exports = {
   ftoc,
   ctof

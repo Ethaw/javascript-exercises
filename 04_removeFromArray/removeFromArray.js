c36180605654f0d383b608f754e31c32634927ef
Ethaw <12903109+Ethaw@users.noreply.github.com>
2022-11-06 02:54:31 +0100

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+// Note: values are all considered unique.
 
+const removeFromArray0 = function(arr, ...values) {
+  // Naive way, iteratively removing each value in place
+  for (val of values) {
+    let i = arr.indexOf(val);
+    if (i >= 0) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr
 };
 
-// Do not edit below this line
+const removeFromArray = function(arr, ...values) {
+  // Using filter and set
+  values = new Set(values);
+  return arr.filter(x => !(values.has(x)));;
+};
+
 module.exports = removeFromArray;

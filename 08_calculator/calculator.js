292173d8ac6a61ac9f423068ef99555854339644
Ethaw <12903109+Ethaw@users.noreply.github.com>
2022-11-06 04:52:04 +0100

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,28 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((x, y) => x + y, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((x, y) => x * y, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  if (x === 0 && y === 0) {
+    return 0;
+  }
+	let res = 1;
+  for (let i=0; i<y; i++) {
+    res *= x;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let res = 1;
+  for (let i=2; i<=n; i++) {
+    res *= i;
+  }
+  return res;
 };
 
-// Do not edit below this line
 module.exports = {
   add,
   subtract,

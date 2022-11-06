38543ef254885a62a64e4fe2ea06cfe384b80dfa
Ethaw <12903109+Ethaw@users.noreply.github.com>
2022-11-06 03:39:52 +0100

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+  const isInvalidArg = x => {
+    return (typeof x !== 'number' || parseInt(x) !== x || x < 0 
+            ? true : false)
+  }
 
+  if (isInvalidArg(x) || isInvalidArg(y)) {
+    return 'ERROR';
+  }
+
+  if (x > y) {
+    [x, y] = [y, x];
+  }
+
+  // Using maths to solve in O(1)
+  const f = x => x*(x+1)/2;
+  return f(y) - f(x-1);
+
+  // // Using a loop
+  // let sum = y;
+  // for (; x<y; x++) {
+  //   sum += x;
+  // }
+  // return sum
 };
 
-// Do not edit below this line
 module.exports = sumAll;

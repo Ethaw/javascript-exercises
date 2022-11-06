9e3fa5a604dbf8c4dec9c0e8a1f522910a505ed4
Ethaw <12903109+Ethaw@users.noreply.github.com>
2022-11-06 01:22:10 +0100

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  num = parseInt(num);
+  if (num === NaN || num < 0) {
+    return 'ERROR';
+  }
 
+  // Using built-in.
+  return string.repeat(num);
+
+  // Manually with join.
+  let arr = [];
+  for (let i=0; i<num; i++) {
+    arr.push(string);
+  }
+  return arr.join('');
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
